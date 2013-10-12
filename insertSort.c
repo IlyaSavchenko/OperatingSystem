a4fa8905b855a83a2de6e7844b730f45de3fc879
IlyaSavchenko <sav.il.yu@gmail.com>
2013-10-13 01:14:32 +0400

    Update insertSort.c

--- insertSort.c
+++ insertSort.c
@@ -1,11 +1,18 @@
 #include <stdio.h>
 #include <pthread.h>
-#include <time.h>
+#include <stdlib.h>
+#include <sys/types.h> 
+#include <unistd.h>
+#include <wait.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+
 
 void readFile();
-void insertSort(int start, int end);
+void sort(int start, int end);
 static void *threadFunc(void *vptr_args);
-void recordFile();
+void mergeArray(int *newArr);
+void recordFile(int *arr);
 void recordFile1();
 int* array;
 int length;
@@ -18,53 +25,44 @@ struct parms
 
 main()
 {
-	clock_t timer;
-	clock_t timer1;
 	readFile();
 	struct parms first = {0, length / 2};
 	struct parms second = {length / 2, length};
-	struct parms result = {0, length};
-	timer = clock();
+	struct timespec startThread;
+    clock_gettime(1, &startThread);
 	pthread_create(&firstThr, NULL, threadFunc, &first);
 	pthread_create(&secondThr, NULL, threadFunc, &second);
 	pthread_join(firstThr, NULL);
 	pthread_join(secondThr, NULL);
-	pthread_create(&resultThr, NULL, threadFunc, &result);
-	timer = clock() - timer;
-	pthread_join(resultThr, NULL);
-	printf("Time for thread sort: %f\n", (double)timer/CLOCKS_PER_SEC);
-	//printf("\n");
-	recordFile();
-    	delete[] array;
+	int *final = new int[length];
+	mergeArray(final);
+	struct timespec stopThread;
+	clock_gettime(1, &stopThread);
+    printf("ThreadSort: %d sec %lu nanosec\n", abs(stopThread.tv_sec - startThread.tv_sec), labs(stopThread.tv_nsec - startThread.tv_nsec));
+    recordFile(final);
 	
 	readFile();
-	timer1 = clock();
-	insertSort(result.left, result.right);
-	timer1 = clock() - timer1;
-	printf("Time for sort: %f\n", (double)timer1/CLOCKS_PER_SEC);
+	struct timespec startSort;
+    clock_gettime(1, &startSort);
+	sort(0, length);
+	struct timespec stopSort;
+	clock_gettime(1, &stopSort);
+    printf("ThreadSort: %d sec %lu nanosec\n", abs(stopSort.tv_sec - startSort.tv_sec), labs(stopSort.tv_nsec - startSort.tv_nsec));
 	recordFile1();
-	delete[] array;
 }
 
 static void *threadFunc(void *vptr_args)
 {
 	struct parms *arg = (struct parms *)vptr_args;
-	insertSort(arg->left, arg->right);
+	sort(arg->left, arg->right);
 
 }
-
-
-// static void *firstPart(void *vptr_args)
-// {
-// 	insertSort(0, (p.length - 1) / 2);
-
-// }
 
 	
 void readFile()
 {
 	FILE *f;
-	f = fopen("in.txt", "rt");
+	f = fopen("./in.txt", "rt");
 	if(!f)
    	{
     	printf("Can't open in.txt\n");
@@ -82,33 +80,58 @@ void readFile()
 }
 
 
-
-void insertSort(int start, int end)
+void sort(int start, int end)
 {
-	// struct params *first = arg;
-	// struct params *second = arg;
-	// struct params *result = arg;
-	//params = (struct parms_t)*arg;
-	//int len = end - start + 1;
- 	int i, j, temp;
-      for (i = start; i < end; i++)
-      {
-            temp = array[i];
-            for (j = i - 1; j >= start; j--)
-            {
-                  if (array[j] < temp)
-                  break;
-                  array[j + 1] = array[j];
-                  array[j] = temp;
-            }   
-      }
+	// //int len = end - start + 1;
+ 	// int i, j, temp;
+  //     for (i = start; i < end; i++)
+  //     {
+  //           temp = array[i];
+  //           for (j = i - 1; j >= start; j--)
+  //           {
+  //                 if (array[j] < temp)
+  //                 break;
+  //                 array[j + 1] = array[j];
+  //                 array[j] = temp;
+  //           }   
+  //     }
+	for (int i = start; i < end - 1; ++i) {
+/* устанавливаем начальное значение минимального индекса */
+        int min_i = i;
+        /* находим индекс минимального элемента */
+        for (int j = i + 1; j < end; ++j) {
+                if (array[j] < array[min_i]) {
+                        min_i = j;
+                }
+        }
+        /* меняем значения местами */
+        int temp = array[i];
+        array[i] = array[min_i];
+        array[min_i] = temp;
+}
 }
 
 
-void recordFile()
+	void mergeArray(int *newArr){
+	int c1 = 0, c2 = length / 2;
+    for (int i = 0; i < length; i++) {
+        if (c1 < length / 2 && c2 <= length) {
+            newArr[i] = array[c1] > array[c2] ? array[c2++] : array[c1++];
+        }	
+        if (c1 > length / 2 && c2 <= length) {
+            newArr[i] = array[c2++];
+        }
+        if (c1 <= length / 2 && c2 > length) {
+            newArr[i] = array[c1++];
+        }
+    }
+}
+
+
+void recordFile(int *arr)
 {
 	FILE *f;
-	f = fopen("out.txt", "wt");
+	f = fopen("./out.txt", "wt");
 	if(!f)
    {
       printf("Can't open out.txt\n");
@@ -119,7 +142,7 @@ void recordFile()
 		fprintf(f, "\n");
 		for(int i = 0; i < length; i++)
 		{
-			fprintf(f, "%d", array[i]);
+			fprintf(f, "%d", arr[i]);
 			fprintf(f, " ");
 		}
 	}
@@ -128,7 +151,7 @@ void recordFile()
 void recordFile1()
 {
 	FILE *f;
-	f = fopen("out1.txt", "wt");
+	f = fopen("./out1.txt", "wt");
 	if(!f)
    {
       printf("Can't open out.txt\n");
@@ -144,4 +167,3 @@ void recordFile1()
 		}
 	}
 }
-
